60793a08569c15a613ed6546a05a54caaee29d79
CherikoM <bakabakaKikichan@gmail.com>
2023-12-13 18:57:23 +0800

    Site updated: 2023-12-13 18:57:07

---
+++ extra/lyric/js/data.js
@@ -0,0 +1,22 @@
+export default `[00:13.445]Digital affection
+[00:17.693]Mechanical perfection
+[00:20.688]I was made for you
+[00:26.691]Outer world dimension
+[00:30.185]Hardwired connection
+[00:38.124]We can have it all tonight
+[00:38.623]
+[00:39.122]Nothing matters when I'm with you baby
+[00:41.876]Plug me in like your electric lady
+[00:45.825]Nothing matters when I’m with you baby
+[00:47.833]Plug me in like your electric lady
+[01:01.324]Let me in like you electric baby
+[01:19.074]
+[01:19.331]Electric
+[01:23.825]Nothing matters when I'm with you baby
+[01:26.574]Let me in like you electric baby
+[01:29.581]
+[01:40.333]Let me in like you electric baby
+[01:55.833]
+[01:56.331]Your electric
+[02:02.577]Nothing matters when I'm with you baby
+[02:05.073]Let me in like you electric baby`
